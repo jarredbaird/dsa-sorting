a90ba20e5da460e33d1ed96d18b4feb1aa20723b
Jarred Baird <jarredbaird@gmail.com>
2021-10-05 16:08:56 -0400

    final submission

--- merge.js
+++ merge.js
@@ -1,5 +1,92 @@
-function merge() {}
+/**
+ * Given two sorted arrays, write a function called merge which accepts two sorted arrays and returns a new array with values from both arrays sorted.
+ *
+ * This function should run in O(n + m) time and O(n + m) space and should not modify the parameters passed to it.
+ *
+ * Also, do not use the built in .sort() method! We’re going to use this function to implement a sort, so the helper itself shouldn’t depend on a sort.
+ *
+ * let arr1 = [1, 3, 4, 5];
+ * let arr2 = [2, 4, 6, 8];
+ * merge(arr1, arr2); // [1,2,3,4,4,5,6,8]
+ *
+ * arr1; // [1,3,4,5];
+ * arr2; // [2,4,6,8];
+ *
+ * let arr3 = [-2, -1, 0, 4, 5, 6];
+ * let arr4 = [-3, -2, -1, 2, 3, 5, 7, 8];
+ *
+ * merge(arr3, arr4); // [-3,-2,-2,-1,-1,0,2,3,4,5,5,6,7,8]
+ *
+ * let arr5 = [3, 4, 5];
+ * let arr6 = [1, 2];
+ *
+ * merge(arr5, arr6); // [1,2,3,4,5] */
 
-function mergeSort() {}
+function merge(arr1, arr2) {
+  const mergedArr = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      mergedArr.push(arr1[i]);
+      i++;
+    } else {
+      mergedArr.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    mergedArr.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    mergedArr.push(arr2[j]);
+    j++;
+  }
+  return mergedArr;
+}
 
-module.exports = { merge, mergeSort};
+/**
+ * Implement the merge sort algorithm. Given an array, this
+ * algorithm will sort the values in the array. Here’s some
+ * guidance for how merge sort should work:  Break up the
+ * array into halves until you can compare one value with
+ * another Once you have smaller sorted arrays, merge those
+ * with other sorted pairs until you are back at the
+ * full length of the array Once the array is merged back
+ * together, return the merged (and sorted!) array In
+ * order to implement this function, you’ll also need to
+ * implement a merge function that takes in two sorted
+ * arrays and returns a new sorted array. You implemented
+ * this function in the previous exercise, so use that
+ * function! You can read more about Merge Sort
+ *
+ * mergeSort([4, 20, 12, 10, 7, 9]); // [4, 7, 9, 10, 12, 20]
+ *
+ * mergeSort([0, -10, 7, 4]); // [-10, 0, 4, 7]
+ * mergeSort([1, 2, 3]); // [1, 2, 3]
+ * mergeSort([]);
+ *
+ * let nums = [
+ *   4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342,
+ *   32,
+ * ];
+ *
+ * mergeSort(nums); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35,
+ * //  43, 67, 75, 232, 232, 453, 546, 4342] */
+
+function mergeSort(arr, pass = 0) {
+  pass++;
+
+  debugger;
+  // Base Case
+  if (arr.length <= 1) return arr;
+
+  // Normal Case
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid), pass);
+  const right = mergeSort(arr.slice(mid), pass);
+  return merge(left, right);
+}
+
+module.exports = { merge, mergeSort };
